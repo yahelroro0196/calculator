d25769c4fb08bef006a6b3ffee5f142a8dc4e19d
Yahel Rosen <yahelu7@gmail.com>
2020-04-20 15:09:15 +0300

    deleted useless space in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,7 @@ import java.util.ArrayList;
 import static com.company.Calculator.CalculatorGui.menu;
 
 public class Main {
-
-
     public static void main(String[] args) {
         menu();
     }
-
 }
