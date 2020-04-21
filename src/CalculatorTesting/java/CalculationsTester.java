486b4f0e7937e5bf6f07cf4b92b538be3251752b
Yahel Rosen <thederociteamhd@gmail.com>
2020-04-21 17:19:18 +0300

    added example of edge case test in separate class.

--- src/CalculatorTesting/java/CalculationsTester.java
+++ src/CalculatorTesting/java/CalculationsTester.java
@@ -1,50 +1,30 @@
 import ShuntingYardCalculator.Calculator.CalculationSteps.InfixToPostfix;
 import ShuntingYardCalculator.Calculator.CalculationSteps.PostfixToResult;
-import ShuntingYardCalculator.Type;
-import javafx.util.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import EdgeCases.InfixToPostfixCases;
 
 public class CalculationsTester {
 
     public static final int postfixToResultOutput = 65;
-    private ArrayList<Pair<String, Type>> infixToPostfixInput;
-    private ArrayList<Pair<String, Type>> infixToPostfixOutput;
 
     @Before
-    public void prepareTests() {
-        infixToPostfixOutput = new ArrayList<>();
-        infixToPostfixInput = new ArrayList<>();
-        infixToPostfixOutput.add(new Pair<>("5", Type.OPERAND));
-        infixToPostfixOutput.add(new Pair<>("5", Type.OPERAND));
-        infixToPostfixOutput.add(new Pair<>("+", Type.OPERATOR));
-        infixToPostfixOutput.add(new Pair<>("55", Type.OPERAND));
-        infixToPostfixOutput.add(new Pair<>("+", Type.OPERATOR));
-        infixToPostfixInput.add(new Pair<>("5", Type.OPERAND));
-        infixToPostfixInput.add(new Pair<>("+", Type.OPERATOR));
-        infixToPostfixInput.add(new Pair<>("5", Type.OPERAND));
-        infixToPostfixInput.add(new Pair<>("+", Type.OPERATOR));
-        infixToPostfixInput.add(new Pair<>("55", Type.OPERAND));
+    public void programWideTestsPreparation() {
+        InfixToPostfixCases.inputSetup();
+        InfixToPostfixCases.outputSetup();
     }
 
     @Test
-    public void infixToPostfixTester() {
-        assertEquals(infixToPostfixOutput, InfixToPostfix.infixToPostfix(infixToPostfixInput));
-    }
-
-    @Test
-    public void postfixToResultTester() {
-        assertEquals(postfixToResultOutput, PostfixToResult.postfixToResult(infixToPostfixOutput), 0.01);
+    public void programWideTests() {
+        InfixToPostfixCases.doubleBrackets();
     }
 
     @After
     public void finalize_test() {
-        infixToPostfixOutput.clear();
-        infixToPostfixInput.clear();
+        InfixToPostfixCases.finalize_tests();
     }
 }
